601f22a7deb6e16e40a49885aa90535dc99f85d1
unknown <arojasgomez.93@gmail.com>
2025-05-10 19:57:14 -0300

    Fix: Postulante

--- 4. Archivos/ArchivosPostulantes/Postulante.cpp
+++ 4. Archivos/ArchivosPostulantes/Postulante.cpp
@@ -1,26 +1,59 @@
 #include "Postulante.h"
+#include <string>
+///Para copiar vectores de char se usa strcpy con esta libreria de C para los strings
+#include <cstring>
+using namespace std;
+
+Postulante::Postulante()
+{
+    /// NO PUEDES ASIGNAR UN VECTOR DE CHAR COMO _apellidos = ""
+    _id = 0;
+    strcpy(_apellidos,"");
+    strcpy(_nombres,"");
+    _edad = 0;
+    strcpy(_puesto,"");
+    _aniosExperiencia = 0;
+    _isContratado = false;
+}
+
+Postulante::Postulante(int id, string apellidos, string nombres, int edad, string puesto, int aniosExperiencia, bool isContratado)
+{
+    setID(id);
+    setApellidos(apellidos);
+    setNombres(nombres);
+    setEdad(edad);
+    setPuesto(puesto);
+    setAniosExperiencia(aniosExperiencia);
+    setIsContratado(isContratado);
+
+}
 
 int Postulante::getID()
 {
     return _id;
 }
 
-char * Postulante::getApellidos()
+void Postulante::setID(int id)
+{
+    _id = id;
+}
+
+string Postulante::getApellidos()
 {
     return _apellidos;
 }
-void Postulante::setApellidos(char * apellidos)
+void Postulante::setApellidos(string apellidos)
 {
-    _apellidos = apellidos;
+    strcpy(_apellidos,apellidos.c_str());
 }
 
-char * Postulante::getNombres()
+string Postulante::getNombres()
 {
     return _nombres;
 }
-void Postulante::setNombres(char * nombres)
+void Postulante::setNombres(string nombres)
 {
-    _nombres = nombres;
+    strcpy(_nombres,nombres.c_str());
 }
 
 int Postulante::getEdad()
@@ -32,13 +65,13 @@ void Postulante::setEdad(int edad)
     _edad = edad;
 }
 
-char * Postulante::getPuesto()
+string Postulante::getPuesto()
 {
     return _puesto;
 }
-void Postulante::setPuesto(char * puesto)
+void Postulante::setPuesto(string puesto)
 {
-    _puesto = puesto;
+    strcpy(_puesto,puesto.c_str());
 }
 
 int Postulante::getAniosExperiencia()
@@ -49,3 +82,13 @@ void Postulante::setAniosExperiencia(int aniosExperiencia)
 {
     _aniosExperiencia = aniosExperiencia;
 }
+
+bool Postulante::getIsContratado()
+{
+    return _isContratado;
+}
+void Postulante::setIsContratado(bool isContratado)
+{
+    _isContratado = isContratado;
+}
+
--- 4. Archivos/ArchivosPostulantes/Postulante.h
+++ 4. Archivos/ArchivosPostulantes/Postulante.h
@@ -1,33 +1,41 @@
 #pragma once
+#include <string>
 
 class Postulante
 {
 private:
     int _id;
-    char _apellidos[50]
+    char _apellidos[50];
     char _nombres[50];
     int _edad;
     char _puesto[50];
     int _aniosExperiencia;
+    bool _isContratado;
 
 
 public:
+    Postulante();
+    Postulante(int id, std::string apellidos, std::string nombres, int edad, std::string puesto, int aniosExperiencia, bool isContratado);
+
     int getID();
 
-    char * getApellidos();
-    void setApellidos(char * apellidos);
+    std::string getApellidos();
+    void setApellidos(std::string apellidos);
 
-    char * getNombres();
-    void setNombres(char * nombres);
+    std::string getNombres();
+    void setNombres(std::string nombres);
 
     int getEdad();
     void setEdad(int edad);
 
-    char * getPuesto();
-    void setPuesto(char * puesto);
+    std::string getPuesto();
+    void setPuesto(std::string puesto);
 
     int getAniosExperiencia();
     void setAniosExperiencia(int aniosExperiencia);
+
+    bool getIsContratado();
+    void setIsContratado(bool isContratado);
 
 };
 
